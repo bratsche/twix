9971e9784f15def303b076b3175eafba52e772fa
Cody Russell <cody@jhu.edu>
2022-12-22 23:50:33 -0500

    [hex] Prepare to publish.

--- mix.exs
+++ mix.exs
@@ -1,28 +1,51 @@
 defmodule Twix.MixProject do
   use Mix.Project
 
-  def project do
+  @version "0.1.0"
+  @description "Tailwind class merging in Elixir"
+
+  def project() do
     [
       app: :twix,
-      version: "0.1.0",
+      version: @version,
+      description: @description,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package(),
+      source_url: "https://github.com/bratsche/twix"
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
-  def application do
+  def application() do
     [
-      extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp deps() do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "Twix",
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/bratsche/twix",
+      extras: ["CHANGELOG.md"],
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
+    ]
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/bratsche/twix"
+      }
     ]
   end
 end
